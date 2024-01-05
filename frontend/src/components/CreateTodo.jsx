871777d81eb6f55e05e2c6effa22cf399f5e79fd
Debjyoti Banerjee <deb07ban@gmail.com>
2024-01-05 12:55:32 +0530

    updated react file

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,51 +1,80 @@
-import { react, useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export function CreateTodo(props){
-    const [title,setTitle] = useState("");
-    const[description,setDescription]=useState("");
+export function CreateTodo(props) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    return <div>
-        <input style={{
-            margin:5,
-            padding:10,
+  const addTodo = () => {
+    axios
+      .post(
+        "http://localhost:3000/todo",
+        {
+          title: title,
+          description: description,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        alert("Todo added");
+      })
+      .catch((error) => {
+        console.error("Error adding todo:", error);
+        alert("Error adding todo");
+      });
+  };
+
+  const showTodo = () => {
+    axios
+      .get("http://localhost:3000/todo")
+      .then((response) => {
+        console.log("Todos:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error showing todos:", error);
+        alert("Error showing todos");
+      });
+  };
+
+  return (
+    <div>
+      <input
+        style={{ padding: 10, margin: 10 }}
+        id="title"
+        type="text"
+        placeholder="title"
+        onChange={(e) => {
+          const value = e.target.value;
+          setTitle(value);
         }}
-        id="title" type="text" placeholder="title" onChange={function(e){
-            const value = e.target.value;
-            setTitle(value);
-        }} ></input><br></br>
-        <input style={{
-            margin:5,
-            padding:10,
-        }} id="title" type="text" placeholder="description" onChange={function(e){
-            const value = e.target.value;
-            setDescription(value);
-        }} ></input><br></br>
-        <button style={{
-            margin:5,
-            padding:10,
+      ></input>
+      <br></br>
+
+      <input
+        style={{ padding: 10, margin: 10 }}
+        id="title"
+        type="text"
+        placeholder="description"
+        onChange={(e) => {
+          const value = e.target.value;
+          setDescription(value);
         }}
-        onClick={() => {
-            fetch("http://localhost:3000/todo", {
-                method: "POST",
-                body: JSON.stringify({
-                    title: title,
-                    description: description,
-                }),
-                headers: {
-                    "Content-type": "application/json",
-                },
-            })
-            .then(async function (res) {
-                if (!res.ok) {
-                    throw new Error(`HTTP error! Status: ${res.status}`);
-                }
-                const json = await res.json();
-                alert("Todo added");
-            })
-            .catch((error) => {
-                console.error("Error adding todo:", error);
-                alert("Error adding todo");
-            });
-        }}>Add todo</button>
+      ></input>
+      <br></br>
+
+      <button style={{ padding: 10, margin: 10 }} onClick={addTodo}>
+        Add todo
+      </button>
+      <br></br>
+
+      <button style={{ padding: 10, margin: 10 }} onClick={showTodo}>
+        Show todos
+      </button>
+      <br></br>
     </div>
+  );
 }
